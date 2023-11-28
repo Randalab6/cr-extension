ba3e773debeb135f22247d0ea36f8d95d665b8ce
randalab6 <randalab6@gmail.com>
2023-11-27 19:42:43 -0600

    Update MongoDB connection string to use environment variable

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const path = require('path');
 const mongoose = require("mongoose")
 require('dotenv').config();
 const cors = require("cors")
+
+const MONG0_URL = process.env.MONGODB_ATLAS_URL
+
+mongoose.connect(MONG0_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+   .then(() => console.log("Database connected successfully!"));
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -10,7 +14,6 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-
 app.listen(PORT, () => {
-    console.log('Server started on port', PORT);
+   console.log('Server started on port', PORT);
 });
