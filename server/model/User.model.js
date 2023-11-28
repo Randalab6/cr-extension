7819948688e295406e1c8bad8a8537d9f93fe15a
randalab6 <randalab6@gmail.com>
2023-11-27 20:05:04 -0600

    Implement User model with likeSchema in MongoDB

---
+++ server/model/User.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+const likeSchema = new mongoose.Schema({  
+    quotation: String,
+    author: String,
+    image: String,
+    imageSize: {
+        height: Number,
+        width: Number
+    }
+})
+
+const userSchema = new mongoose.Schema({
+    userId: { type: String, unique: true },
+    likes: [likeSchema]
+})
+
+module.exports = mongoose.model("User", userSchema)  
