f179c6f8d3a95a0d446ff45d24da7d9332a9ea54
randalab6 <randalab6@gmail.com>
2023-11-28 16:29:39 -0600

    Add likeRouter to handle '/likes' route in Express app

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose")
 require('dotenv').config();
 const cors = require("cors")
 
+// import like Router
+const likeRouter = require("./routes/like.router")
+
 const MONG0_URL = process.env.MONGODB_ATLAS_URL
 
 mongoose.connect(MONG0_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
@@ -13,6 +16,9 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+
+// use like Router to handle requests to "/likes" route
+app.use("/likes", likeRouter)
 
 app.listen(PORT, () => {
    console.log('Server started on port', PORT);
