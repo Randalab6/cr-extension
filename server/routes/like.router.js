344bf364005bfbc3d2872f8ec23baeebd65060d9
randalab6 <randalab6@gmail.com>
2023-11-28 20:27:04 -0600

    Add route to get a specific user's likes by userId

--- server/routes/like.router.js
+++ server/routes/like.router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const { likeAFact } = require("../controllers/like.controller")
 
 router.post("/", likeAFact)
+router.get("/:userId", getAUser)
 
 module.exports = router;
