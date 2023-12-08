7e82f2dcf5512c753ae647fa283d8539dfa35e76
randalab6 <randalab6@gmail.com>
2023-12-08 11:54:10 -0600

    Convert apis.js to TypeScript and Update Dependencies

--- client/src/utils/apis.ts
+++ client/src/utils/apis.ts
@@ -2,5 +2,5 @@ import { createApi } from "unsplash-js";
 
 export const FACT_API = "https://history.muffinlabs.com/date"
 export const unsplash = createApi({
-    accessKey: process.env.REACT_APP_UNSPLASH_SECRET_KEY
+    accessKey: process.env.REACT_APP_UNSPLASH_SECRET_KEY as string
 })
